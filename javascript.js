753a91dd4d2d0e6acea85910f13a0b81f17ae1d6
wondwizard <148422096+wondwizard@users.noreply.github.com>
2024-03-16 07:12:47 -0500

    added button to create new grid and the javascript required to create the grid

--- javascript.js
+++ javascript.js
@@ -2,8 +2,9 @@ window.onload = function () {
 
 
     const squareContainer = document.getElementById('container');
+    const btn = document.getElementById('newGridBtn');
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.setAttribute('id', 'square');
         div.style.cssText = 'border-style: solid; border-width: 3px; width: 50px; height: 50px; margin: 5px;'
@@ -11,6 +12,35 @@ window.onload = function () {
         
     }
 
+    function createNewGrid(num) {
+        while (squareContainer.lastElementChild) {
+            squareContainer.removeChild(squareContainer.lastElementChild);
+        }
+        for (let i = 0; i < (num*num); i++) {
+            const div = document.createElement('div');
+            div.setAttribute('id', 'square');
+            div.style.cssText = 'border-style: solid; border-width: 3px; width: 50px; height: 50px; margin: 5px;'
+            squareContainer.appendChild(div);
+            
+        }
+        squareDiv = document.querySelectorAll('#square');
+        squareDiv.forEach(squareDiv => squareDiv.addEventListener('mouseover', () => {
+            squareDiv.style.backgroundColor = 'black';
+        }));
+    }
+
+    let squareDiv = document.querySelectorAll('#square');
+
+    btn.addEventListener('click', () => {
+        createNewGrid(parseInt(prompt('Enter a number')));
+    });
+
+    squareDiv.forEach(squareDiv => squareDiv.addEventListener('mouseover', () => {
+        squareDiv.style.backgroundColor = 'black';
+    }));
+
+
+   
 
 
     
