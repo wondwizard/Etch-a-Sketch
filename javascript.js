de88f552f23a19b5e9b5531682f14ad4c1fbdded
wondwizard <148422096+wondwizard@users.noreply.github.com>
2024-03-16 08:10:05 -0500

    got the squares to be created properly

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ window.onload = function () {
 
     const squareContainer = document.getElementById('container');
     const btn = document.getElementById('newGridBtn');
+    const gridWidth = 1100
 
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
@@ -13,16 +14,20 @@ window.onload = function () {
     }
 
     function createNewGrid(num) {
+     
         while (squareContainer.lastElementChild) {
             squareContainer.removeChild(squareContainer.lastElementChild);
         }
         for (let i = 0; i < (num*num); i++) {
             const div = document.createElement('div');
             div.setAttribute('id', 'square');
-            div.style.cssText = 'border-style: solid; border-width: 3px; width: 50px; height: 50px; margin: 5px;'
-            squareContainer.appendChild(div);
-            
+            div.style.width = `${gridWidth/num}px`;
+            div.style.height = `${gridWidth/num}px`;
+            div.style.backgroundColor = 'blue';
+            div.style.outline = '5px solid red';
+            squareContainer.appendChild(div);  
         }
+        
         squareDiv = document.querySelectorAll('#square');
         squareDiv.forEach(squareDiv => squareDiv.addEventListener('mouseover', () => {
             squareDiv.style.backgroundColor = 'black';
